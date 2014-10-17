cffb0dddc8ce5c23c5f83c0190636a6fc7a84c40
Samuel-Graham <samgraham@outlook.com>
2014-10-17 15:22:17 -0600

    Added a code question from an interview I had recently

---
+++ numberSwap.cpp
@@ -0,0 +1,19 @@
+/***************
+* Sam Graham
+* This is from an interview I had recently.
+* The goal is to switch around and integer using only math operations
+* no strings allowed!!
+****************/
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char** argv) {
+  int pVal = atoi(argv[1]);
+  while( pVal > 0 ) {
+    cout << pVal % 10;
+    pVal = pVal / 10;
+  }
+  cout << endl;
+  return 0;
+}
